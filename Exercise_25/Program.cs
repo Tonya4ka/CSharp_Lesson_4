0b4cc0660e7db48df38a9c36cdf77fedaac1d086
tonyatrushkova <antonina.trushkova@gmail.com>
2022-09-28 23:47:11 +0300

    Add Exercise_25 + gitignore

---
+++ Exercise_25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Clear();
+Console.Write("Type a number A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Type a number B: ");
+int b = int.Parse(Console.ReadLine()!);
+if (b < 0)
+   Console.Write("The number B could not be negative");
+else
+{
+   int power = Power(a, b);
+   Console.Write($"{a} to the power of {b} is {power}");
+}
+
+int Power(int a, int b)
+{ 
+   int result = 1;
+   for(int i = 1; i<= b; i++)
+   {
+     result = result * a;
+   }
+   return result;
+}
